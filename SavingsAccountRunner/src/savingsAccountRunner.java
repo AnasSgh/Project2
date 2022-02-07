91d8bebf4d2b1a8503073946cdf6b148eac56b7f
A.Sghiouer <99154362+AnasSgh@users.noreply.github.com>
2022-02-06 21:33:31 -0600

    Add files via upload

---
+++ SavingsAccountRunner/src/savingsAccountRunner.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class savingsAccountRunner {
+
+	public static void main(String[] args) {
+		
+		Scanner input =new Scanner(System.in);
+		
+		System.out.println("Welcome to your Savings ASccount App!");
+		
+		System.out.println("Enter your Account Number :");
+		
+		int id= input.nextInt();
+	
+		
+		SavingAccount Account = new SavingAccount();
+		
+	}
+	
+	
+
+}
+ 
